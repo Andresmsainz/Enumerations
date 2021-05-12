c7be1438f2c28397505b7d088f04278a3c08b7d4
Andres Sainz <andresmsainz@gmail.com>
2021-05-11 23:02:38 -0400

    initial commit

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Enumeration
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enumerations Boyeee!");
+            string weekDay = WeekDays.Friday.ToString();
+            Console.WriteLine("The day (to string) is " + weekDay);
+
+            WeekDays day = WeekDays.Saturday;
+            Console.WriteLine("The day (property) is " + day);
+            Console.WriteLine("The day (integer property) is " + (int)day);
+
+        }
+    }
+
+    enum WeekDays
+    {
+        Monday,
+        Tuesday,
+        Wednesday,
+        Thursday,
+        Friday,
+        Saturday,
+        Sunday
+    }
+}
